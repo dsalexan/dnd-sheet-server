5b9caef16d0f42e7240c233b3768ce5f86866d53
dsalexan <danilo.salexan@gmail.com>
2019-08-02 19:15:10 -0300

    adicionando endpoint de busca de slugs

--- index.js
+++ index.js
@@ -68,20 +68,94 @@ server.get('/classes', (req, res) => {
     let q = req.query.q
     debug(`CLASSES ${q}`)
 
-    global.database.collection('resources').find({
-        '$or': [
-            {
-                name: q
-            },
-            {
+    global.database.collection('resources').aggregate([
+        {
+          '$match': {
+            '$or': [
+              {
+                'name': q
+              }, {
                 'name.en': q
-            },
-            {
+              }, {
                 'name.pt-BR': q
+              }
+            ]
+          }
+        }, {
+          '$lookup': {
+            'from': 'resources', 
+            'localField': '_id', 
+            'foreignField': 'parent', 
+            'as': 'features'
+          }
+        }, {
+          '$project': {
+            'meta': 1, 
+            'name': 1, 
+            'subscriptions': 1, 
+            'mechanics': 1, 
+            'features': {
+              '$filter': {
+                'input': '$features', 
+                'as': 'resource', 
+                'cond': {
+                  '$eq': [
+                    '$$resource.meta', 'feature'
+                  ]
+                }
+              }
             }
-        ]
-    }).toArray((err, docs) => {
+          }
+        }
+      ]).toArray((err, docs) => {
         debug('CLASS entry found')
         res.send(docs)
     })
+})
+
+server.get('/', (req, res) => {
+  let q = req.query.q
+  debug(`FIND RESOURCE ${q}`)
+
+  global.database.collection(`slugs`).aggregate([
+    {
+      '$match': {
+        'path': {
+          '$regex': q
+        }
+      }
+    }, {
+      '$lookup': {
+        'from': 'resources', 
+        'localField': '_id', 
+        'foreignField': '_id', 
+        'as': 'resource'
+      }
+    }, {
+      '$addFields': {
+        'resource.path': {
+          '$arrayElemAt': [
+            '$path', 1
+          ]
+        }
+      }
+    }, {
+      '$replaceRoot': {
+        'newRoot': {
+          '$mergeObjects': [
+            {
+              '$arrayElemAt': [
+                '$resource', 0
+              ]
+            }, {
+              'path': '$path'
+            }
+          ]
+        }
+      }
+    }
+  ]).toArray((err, docs) => {
+    debug(`RESOURCE found`)
+    res.send(docs)
+  })
 })
